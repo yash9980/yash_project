a1bf9928dabc457cff89429bc420801aba714ecb
yash9980 <96368970+yash9980@users.noreply.github.com>
2021-12-30 14:57:53 +0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-function setup()
- { createCanvas(400, 400); 
-box=createSprite(200,200,30,30);
-}
-  function draw() 
-  { background("green"); 
-  if(keyDown("right")){
-    box.position.x=box.position.x+5
+
+function draw() {
+  box=createSprite(200,200,30,30)
+  background("yellow");
+  if (keyDown("RIGHT_ARROW")) {
+    background("red");
+    
+    
   }
   drawSprites()
 }
+
